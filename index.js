4a97750d5d4eb5ad9636f6cd7b41160e64c0afcc
Al-amin Sheikh <azalamins@gmail.com>
2022-06-06 12:10:10 +0600

    added appointment post api, get api

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1ba8g.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const appointmentCollection = client
+      .db("AppointmentBooking")
+      .collection("appointment");
+
+    app.post("/appointment", async (req, res) => {
+      const appointmentDetails = req.body;
+      const result = await appointmentCollection.insertOne(appointmentDetails);
+      res.send(result);
+    });
+
+    app.get("/appointment", async (req, res) => {
+      const result = await appointmentCollection.find().toArray();
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
